6baf77513add61f9aa945eec7c6220db31424a82
Kushal Dongre <kushz19@gmail.com>
2012-07-21 15:20:22 +0530

    test

--- db/migrate/20120721032603_add_image_to_product.rb
+++ db/migrate/20120721032603_add_image_to_product.rb
@@ -1,7 +1,7 @@
 class AddImageToProduct < ActiveRecord::Migration
   def change
     add_column :products, :image_file_name, :string 
-    add_column :products, :image_content_type, :string 
-    add_column :products, :image_file_size, :float 
+    add_column :products, :image_content_type, :string
+    add_column :products, :image_file_size, :float
   end
 end
