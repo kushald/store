f69fdf6e608771d4a4aaae50f0423bf3847bff72
Kushal Dongre <kushz19@gmail.com>
2012-07-25 00:42:19 +0530

    user login product list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery
   include SessionsHelper
+  before_filter :current_user
+
+  private
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       sign_in user
       redirect_to "/products"
     else
