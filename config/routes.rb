84947f0b44d6c42c1e2065880e0b661782b8519d
Kushal Dongre <kushz19@gmail.com>
2012-08-19 21:33:06 +0530

    global route

--- config/routes.rb
+++ config/routes.rb
@@ -85,4 +85,5 @@ Store::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match '*a', :to => 'products#index'
 end
