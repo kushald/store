d4bf44424bd24fbd624940f7c818478b995bd8e2
Kushal Dongre <kushz19@gmail.com>
2012-08-28 18:12:18 +0530

    cart remove items via ajax.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,8 @@
 class CartsController < ApplicationController
   def index
+    CartItem.delete_all(["cart_id = ? AND product_id = ?",@cart.id.to_i,params[:product_id]]) if request.xhr? and params[:product_id].present?
     @items = Cart.item_details(:cart_id => @cart.try(:id).to_i)
+    render :layout => false if request.xhr?
   end
   
   # Insert into cart items.
@@ -8,10 +10,10 @@ class CartsController < ApplicationController
     return if params[:product_id].blank?
     @cart_item = CartItem.create(:cart_id => @cart.id,:product_id => params[:product_id])
     
-     respond_to do |format|
-    format.js{
-      render :template => "/carts/insert_into_cart"
-    }
-  end
+    respond_to do |format|
+      format.js{
+        render :template => "/carts/insert_into_cart"
+      }
+    end
   end
 end
