8cfd60507ee9477b91559439bd3fe18a18fe9f55
Kushal Dongre <kushz19@gmail.com>
2012-08-28 22:57:37 +0530

    order of cart items according to updated_at.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,7 +6,7 @@ class Cart < ActiveRecord::Base
   end
   def self.item_details(params)
     sum = 0
-    cart_items = CartItem.where(:cart_id => params[:cart_id]).index_by(&:product_id)
+    cart_items = CartItem.where(:cart_id => params[:cart_id]).order("updated_at desc").index_by(&:product_id)
     products = Product.where("id IN (?)", cart_items.keys).index_by(&:id)
     cart_items.each{|i| sum+= i[1].quantity.to_i}
     {:cart_items => cart_items, :products => products, :quantity => sum.to_i}
