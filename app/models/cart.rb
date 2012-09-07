1c899bb141b37b77dfccf28a2c1930fdd4792c4f
Kushal Dongre <kushz19@gmail.com>
2012-09-08 01:23:20 +0530

    cart refactor

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,9 +8,9 @@ class Cart < ActiveRecord::Base
     total = sum = 0
     cart_items = CartItem.where(:cart_id => params[:cart_id]).order("updated_at desc").index_by(&:product_id)
     products = Product.where("id IN (?)", cart_items.keys).index_by(&:id)
-    cart_items.each do |i| 
-      sum+= i[1].quantity.to_i
-      total+= (products[i[0]].price * i[1].quantity)
+    cart_items.each do |k,v| 
+      sum+= v.quantity.to_i
+      total+= (products[k].price * v.quantity)
     end
     {:cart_items => cart_items, :products => products, :quantity => sum.to_i, :total => total}
   end
