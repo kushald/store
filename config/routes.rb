7168e6dbbad6b05509a1fe59c283826d52bf1fa1
Kushal Dongre <kushz19@gmail.com>
2012-09-15 21:17:31 +0530

    index page changed to product list .

--- config/routes.rb
+++ config/routes.rb
@@ -82,7 +82,7 @@ Store::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'sessions#new'
+   root :to => 'products#index'
 
   # See how all your routes lay out with "rake routes"
 
